9b64890605aa367c17fe6df53bf3b3557663ce7c
A.C.M. <undertakers_help@yahoo.com>
2024-02-16 23:07:44 +0100

    Added currency-shop.

--- init.lua
+++ init.lua
@@ -1,46 +1,61 @@
 -- Registers nodes foreign Nodes for techage with tubes ...
 
-local OwnerCache = {}
+function register_tube(node, main_inventory, pull_inventory, push_inventory)
+    local pull_inventory = pull_inventory or main_inventory
+    local push_inventory = push_inventory or main_inventory
 
--- Check if the chest is in the protected area of the owner
-local function is_owner(pos, meta)
-	local owner = meta:get_string("owner")
-	local key = minetest.hash_node_position(pos)
-	-- If successfull, store info in cache
-	if OwnerCache[key] ~= owner then
-		if not minetest.is_protected(pos, owner) then
-			OwnerCache[key] = owner
-		end
-	end
-	return OwnerCache[key] == owner
-end
+    local OwnerCache = {}
 
+    -- Check if the chest is in the protected area of the owner
+    local function is_owner(pos, meta)
+        local owner = meta:get_string("owner")
+        local key = minetest.hash_node_position(pos)
+        -- If successfull, store info in cache
+        if OwnerCache[key] ~= owner then
+            if not minetest.is_protected(pos, owner) then
+                OwnerCache[key] = owner
+            end
+        end
+        return OwnerCache[key] == owner
+    end
 
-if(minetest.get_modpath("smartshop")) then
-    techage.register_node({"smartshop:wifistorage"}, {
+    techage.register_node({node}, {
         on_inv_request = function(pos, in_dir, access_type)
             local meta = minetest.get_meta(pos)
             if is_owner(pos, meta) then
-                return meta:get_inventory(), "main"
+                return meta:get_inventory(), pull_inventory
             end
         end,
         on_pull_item = function(pos, in_dir, num)
             local meta = minetest.get_meta(pos)
             if is_owner(pos, meta) then
                 local inv = meta:get_inventory()
-                return techage.get_items(pos, inv, "main", num)
+                return techage.get_items(pos, inv, pull_inventory, num)
             end
         end,
         on_push_item = function(pos, in_dir, stack)
             local meta = minetest.get_meta(pos)
             local inv = meta:get_inventory()
-            return techage.put_items(inv, "main", stack)
+            return techage.put_items(inv, push_inventory, stack)
         end,
         on_unpull_item = function(pos, in_dir, stack)
             local meta = minetest.get_meta(pos)
             local inv = meta:get_inventory()
-            return techage.put_items(inv, "main", stack)
+            return techage.put_items(inv, pull_inventory, stack)
         end,
     })
 
 end
+
+if(minetest.get_modpath("smartshop")) then
+    register_tube("smartshop:wifistorage", "main")
+    register_tube("smartshop:shop", "main")
+
+end
+
+
+if(minetest.get_modpath("currency")) then
+    register_tube("currency:shop", "stock")
+
+end
+
